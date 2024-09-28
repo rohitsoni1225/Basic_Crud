65c0b6c2392f78b1f509e5f4e0f34906a6efbb49
Rohit Soni <rohitsonimyr17@gmail.com>
2024-09-28 16:23:57 +0530

    Initial FIles

---
+++ src/app/Forms/Template/tempplate-form/tempplate-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tempplate-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './tempplate-form.component.html',
+  styleUrl: './tempplate-form.component.css'
+})
+export class TempplateFormComponent {
+
+}
